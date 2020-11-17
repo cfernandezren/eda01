6549f97d82e614dbc055acbc31837a85889eebc9
Cristhian Fernandez Ren <72279151+cfernandezren@users.noreply.github.com>
2020-11-16 21:50:38 -0300

    Add files via upload

--- js/calendario.js
+++ js/calendario.js
@@ -6,32 +6,32 @@ document.addEventListener('DOMContentLoaded', function() {
       locale: 'es',
       events: [
             {
-                title: 'Vacuna de Noviembre 1',
-                start: '2020-11-22',
-                end:'2020-11-22'
+                title: 'Vacuna de Noviembre',
+                start: '2020-11-23',
+                end:'2020-11-23'
             },
             {
-                title: 'Vacuna de Diciembre 1',
+                title: 'Vacuna de Diciembre',
                 start: '2020-12-15',
-                end: '2020-12-17',
+                end: '2020-12-15',
 
             },
             {
                 title: 'Vacuna de Enero 1',
                 start: '2021-01-08',
-                end: '2021-01-10',
+                end: '2021-01-08',
 
             },
             {
                 title: 'Vacuna de Enero 2',
                 start: '2021-01-21',
-                end: '2021-01-23',
+                end: '2021-01-21',
 
             },
             {
-                title: 'Vacuna de Febrero 1',
+                title: 'Vacuna de Febrero ',
                 start: '2021-02-07',
-                end: '2021-02-09',
+                end: '2021-02-07',
 
             },
             {
@@ -43,9 +43,24 @@ document.addEventListener('DOMContentLoaded', function() {
             {
                 title: 'Vacuna de Marzo 2',
                 start: '2021-03-25',
-                end: '2021-03-27',
+                end: '2021-03-25',
 
             },
+
+            {
+                title: 'Vacuna de Abril 1',
+                start: '2021-04-08',
+                end: '2021-04-08',
+
+            },
+
+            {
+                title: 'Vacuna de Abril 2',
+                start: '2021-04-20',
+                end: '2021-04-20',
+
+            }
+
         ]
     }); 
 
